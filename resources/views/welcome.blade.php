6c91a8af10e92bdb04f23da9792a89fb838f5def
初コミット <>
2019-06-13 17:50:38 +0900

    トップにマップを埋め込んだ

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,7 +13,33 @@
 
     <main>
         <div class="content">
-            <p>コンテンツ</p>
+    <meta name="viewport" content="initial-scale=1.0">
+    <meta charset="utf-8">
+    <style>
+      #map {
+        height: 100%;
+        width:100%;
+      }
+      html, body {
+        height: 100%;
+        margin: 0;
+        padding: 0;
+      }
+    </style>
+  </head>
+  <body>
+    <div id="map"></div>
+    <script>
+      var map;
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: 35.65786826127019, lng: 139.70091673026695},
+          zoom: 13
+        });
+      }
+    </script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCh0c7Qd5PE6KkiO5TKpcyhNfR3nnwxdjQ&callback=initMap"
+    async defer></script>
         </div>
 
         <div class="localNavigation">
