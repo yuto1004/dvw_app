91c505e340ef4d91f5ab758106aec6e9040f9f85
tech-camp <>
2019-06-14 14:51:25 +0900

    住所からむの追加

---
+++ database/migrations/2019_06_14_054220_add_address_to_shops_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddAddressToShopsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('shops', function (Blueprint $table) {
+            $table->text('address');  //住所カラムの追加
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('shops', function (Blueprint $table) {
+            //
+        });
+    }
+}
