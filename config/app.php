e2a308a46bc870fe9fd87da2a58563291969b331
tech-camp <>
2019-06-18 14:51:46 +0900

    フォームファサード準備

--- config/app.php
+++ config/app.php
@@ -147,6 +147,8 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        // 以下1行を追加
+        Collective\Html\HtmlServiceProvider::class,  // 追加
 
         /*
          * Application Service Providers...
@@ -201,6 +203,9 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        // 以下2行を追加
+        'Form' => Collective\Html\FormFacade::class,  // 追加
+        'Html' => Collective\Html\HtmlFacade::class,  // 追加
 
     ],
 
