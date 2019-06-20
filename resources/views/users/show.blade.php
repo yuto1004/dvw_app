e3274e3a8b485dd886856d9ac9506ba7cb3d539d
tech-camp <>
2019-06-20 16:45:15 +0900

    マイページにデータを表示

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -5,19 +5,24 @@
 <main>
 <div id="form-main">
   <div class="flexbox">
-
+    <h2>{{$name}}さんの投稿一覧</h2>
+    @foreach($reviews as $review)
+    @foreach($shops as $shop)
     <section class="card">
-  <img class="card-img" src="images/bear.jpg" alt="">
-  <div class="card-content">
-    <h1 class="card-title">Webクリエイターボックス</h1>
-    <p class="card-text">ここに投稿文。</p>
-  </div>
-  <div class="card-link">
-    <a href="">About</a>
-    <a href="">Website</a>
-  </div>
-</section>
+      <img class="card-img" src="images/bear.jpg" alt="">
+        <div class="card-content">
+            @if($review->shop_id == $shop->id)
+            <h1 class="card-title">{{$shop->shop_name}}</h1>
 
+            @endif
+          <p class="card-text">{{$review->review}}</p>
+        </div>
+        <div class="card-link">
+          <a href="{{$shop->link}}">Website</a>
+         </div>
+    </section>
+    @endforeach
+    @endforeach
   </div>
 </div>
 </main>
