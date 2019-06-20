3a871ece22184ba00c66df913e11696edb5aa05b
tech-camp <>
2019-06-20 11:31:57 +0900

    ニックネームカラムの削除

---
+++ database/migrations/2019_06_20_022905_remove_nickname_reviews_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class RemoveNicknameReviewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('reviews', function (Blueprint $table) {
+            $table->dropColumn('nickname');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('reviews', function (Blueprint $table) {
+            //
+        });
+    }
+}
