163d53b054831cbc5091300fc86485687d9ea62a
tech-camp <>
2019-06-21 16:14:18 +0900

    コンフリクト対応

--- resources/views/search/top.blade.php
+++ resources/views/search/top.blade.php
@@ -11,8 +11,8 @@
     </div>
     <div class="localNavigation">
       <form id="search" action="/map/search/" method="get">
-        <input id="sbox" name="s" type="text" placeholder="出発駅" /><br>
-        <input id="sbox" name="s" type="text" placeholder="到着駅" />
+        <input id="sbox" name="s[]" type="text" placeholder="出発駅" /><br>
+        <input id="sbox" name="s[]" type="text" placeholder="到着駅" />
         <p class="sbtn">
           <input id="sbtn" type="submit" value="検索">
         </p>
