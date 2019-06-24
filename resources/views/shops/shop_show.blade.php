5766b0e6f085ad59929ecc32944c1e5784bdde4e
tech-camp <>
2019-06-24 17:25:58 +0900

    店ごとにレビュー出しました、全部星５になっちゃいます

--- resources/views/shops/shop_show.blade.php
+++ resources/views/shops/shop_show.blade.php
@@ -2,6 +2,21 @@
 
 @section('content')
 
-
+<ul class="shop_rate" id="list-module">
+        <div class="col">
+            <div class="thumbnail__caption">
+                <h3>{{ $shop->shop_name}}</h3>
+                <span class="rate">
+                    <i class="star-actived rate-{{ round($shop->reviews()->avg('rate')) }}0"></i><br>
+                </span>
+                @foreach($reviews as $review)
+                    <span class="rate">
+                        <i class="star-actived rate-{{ $review->rate }}0"></i>
+                    </span>
+                    <p class="card-text">{{$review->review}}</p>
+                @endforeach
+            </div>
+        </div>
+    </ul>
 
 @endsection
