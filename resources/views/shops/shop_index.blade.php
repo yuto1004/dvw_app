87d3a86faaa56c84be624d8957112ee3fcfa4bad
tech-camp <>
2019-06-25 10:32:36 +0900

    star_complete!!

--- resources/views/shops/shop_index.blade.php
+++ resources/views/shops/shop_index.blade.php
@@ -4,15 +4,8 @@
 
   <ul class="shop_rate" id="list-module">
     @foreach ($shops as $shop)
-      <div class="col">
-        <div class="thumbnail__caption">
-          <h3>
-          <a href="/shops/show/{{ $shop->id }}">{{ $shop->shop_name }}</a>
-          </h3>
-          <span class="rate"></span>
-            <i class="rate{{ round($shop->reviews()->avg('rate')) }}0"></i>
-        </div>
-      </div>
+      <h3><a href="/shops/show/{{ $shop->id }}">{{ $shop->shop_name }}</a></h3>
+      <span class="rate rate{{ round($shop->reviews()->avg('rate')) }}0"></span>
     @endforeach
   </ul>
 
