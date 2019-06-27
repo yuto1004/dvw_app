6450b6c3f0b9b074f0e229fb4e082be5c56029d9
tech-camp <>
2019-06-27 17:01:56 +0900

    a

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use App\Review;
+use App\Shop;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -30,6 +31,29 @@ class DatabaseSeeder extends Seeder
               'created_at' => $data[5],
               'updated_at' => $data[6],
               'images' => $data[7]
+            )
+          );
+        }
+
+
+        $file = new SplFileObject('database/seeds/shops.csv');
+        $file->setFlags(SplFileObject::READ_CSV);
+
+        $ary = array();
+        foreach($file as $data) {
+          array_push($ary, $data);
+        }
+
+        array_shift($ary);
+        foreach($ary as $data) {
+          Shop::create(
+            array(
+              'id' => $data[0],
+              'shop_name' => $data[1],
+              'genre' => $data[4],
+              'link' => $data[5],
+              'address' => $data[8],
+              'avatar' => $data[9],
             )
           );
         }
