6d926d1d9ddef9d78ee2826bd77522c5b742e572
tech-camp <>
2019-06-27 17:13:35 +0900

    a

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Database\Seeder;
-use App\Review;
 use App\Shop;
 class DatabaseSeeder extends Seeder
 {
@@ -12,29 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $file = new SplFileObject('database/seeds/reviews.csv');
-        $file->setFlags(SplFileObject::READ_CSV);
-
-        $ary = array();
-        foreach($file as $data) {
-          array_push($ary, $data);
-        }
-
-        array_shift($ary);
-        foreach($ary as $data) {
-          Review::create(
-            array(
-              'rate' => $data[1],
-              'review' => $data[2],
-              'user_id' => $data[3],
-              'shop_id' => $data[4],
-              'created_at' => $data[5],
-              'updated_at' => $data[6],
-              'images' => $data[7]
-            )
-          );
-        }
-
 
         $file = new SplFileObject('database/seeds/shops.csv');
         $file->setFlags(SplFileObject::READ_CSV);
