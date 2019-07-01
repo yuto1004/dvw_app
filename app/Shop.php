a366631fdce90cfb527e5187aae401d46e7eb4f4
tech-camp <>
2019-07-01 09:58:07 +0900

    timestampsをfalseにしてローカルで動くようにした

--- app/Shop.php
+++ app/Shop.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Shop extends Model
 {
+    public $timestamps = false;
     protected $fillable = array('shop_name','genre','link','address');
 
     public function reviews()
